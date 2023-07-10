7ad7bda19a2e31cc2ea60c9fab8195f186ed96ce
abhishek.k@comviva.com <abhishek.k@comviva.com>
2023-07-10 13:38:53 +0530

    Changes in render system

--- src/components/students.jsx
+++ src/components/students.jsx
@@ -1,3 +1,20 @@
 export const StudentComponent = () => {
-  return <div>Student Components</div>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        backgroundColor: "orange",
+        width: "100%",
+        fontSize: 50,
+        color: "white",
+      }}
+    >
+      <div>Student Component</div>
+    </div>
+  );
 };
